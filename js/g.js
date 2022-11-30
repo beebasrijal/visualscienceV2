e677a6fdf86224a15c9ef24d01042618155df664
Beebas Rijal <beibasrijal@gmail.com>
2022-11-30 22:29:36 +0545

    Repsonsive Earth g

--- js/g.js
+++ js/g.js
@@ -12,55 +12,55 @@ const R = 6400 * 1000;
 const Density = 5464.15;
 const pi = 3.14;
 
-document.querySelector("body").addEventListener("onload", (e) => {
-  resetWindow();
+window.addEventListener("load", (e) => {
+    resetWindow();
 });
 var height = document.querySelector(".height");
 var depth = document.querySelector(".depth");
 var accelerationElem = document.getElementById("acceleration");
 function DisplayAccelerationHeight(h) {
-  var finalHeight = R + h * 1000;
-  var accelerationValue = (G * mass) / Math.pow(finalHeight, 2);
-  accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = ${accelerationValue.toFixed(
-    2
-  )} ms<sup>-2</sup>`;
-  sliderDepth.value = 0;
+    var finalHeight = R + h * 1000;
+    var accelerationValue = (G * mass) / Math.pow(finalHeight, 2);
+    accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = ${accelerationValue.toFixed(
+        2
+    )} ms<sup>-2</sup>`;
+    sliderDepth.value = 0;
 }
 function DisplayAccelerationDepth(d) {
-  var finalDepth = R - d * 1000;
-  var accelerationValue = (4 / 3) * G * Density * finalDepth * pi;
-  accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = ${accelerationValue.toFixed(
-    2
-  )} ms<sup>-2</sup>`;
-  sliderHeight.value = 0;
+    var finalDepth = R - d * 1000;
+    var accelerationValue = (4 / 3) * G * Density * finalDepth * pi;
+    accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = ${accelerationValue.toFixed(
+        2
+    )} ms<sup>-2</sup>`;
+    sliderHeight.value = 0;
 }
 
 sliderHeight.oninput = function () {
-  heightValue = parseInt(this.value);
-  height.innerHTML = heightValue + " km";
-  DisplayAccelerationHeight(heightValue);
-  circle2.style.top = minValue - heightValue / 100 + "%";
-  depth.innerHTML = 0 + " km";
+    heightValue = parseInt(this.value);
+    height.innerHTML = heightValue + " km";
+    DisplayAccelerationHeight(heightValue);
+    circle2.style.top = minValue - heightValue / 100 + "%";
+    depth.innerHTML = 0 + " km";
 };
 
 sliderDepth.oninput = function () {
-  depthValue = parseInt(this.value) * 100;
-  depth.innerHTML = depthValue + " km";
-  DisplayAccelerationDepth(depthValue);
-  circle2.style.top = minValue + 0.00893 * depthValue + "%";
-  height.innerHTML = 0 + " km";
+    depthValue = parseInt(this.value) * 100;
+    depth.innerHTML = depthValue + " km";
+    DisplayAccelerationDepth(depthValue);
+    circle2.style.top = minValue + 0.00893 * depthValue + "%";
+    height.innerHTML = 0 + " km";
 };
 
 const reset = document.getElementById("reset");
 reset.addEventListener("click", (e) => {
-  resetWindow();
+    resetWindow();
 });
 
 function resetWindow() {
-  sliderHeight.value = 0;
-  sliderDepth.value = 0;
-  circle2.style.top = minValue + "%";
-  accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = 9.8 ms<sup>-2</sup>`;
-  sliderHeightValue.innerHTML = 0 + "km";
-  sliderDepthValue.innerHTML = 0 + "km";
+    sliderHeight.value = 0;
+    sliderDepth.value = 0;
+    circle2.style.top = minValue + "%";
+    accelerationElem.innerHTML = `<span class="fd">Acceleration</span> = 9.8 ms<sup>-2</sup>`;
+    sliderHeightValue.innerHTML = 0 + "km";
+    sliderDepthValue.innerHTML = 0 + "km";
 }
